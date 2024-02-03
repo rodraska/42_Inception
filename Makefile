81e346527299b93ea84dacb7adaa38a0cef22d21
rreis-de42 <rreiscarvalho1999@gmail.com>
2024-02-03 08:55:56 -0600

    gg

--- Makefile
+++ Makefile
@@ -1,4 +1,38 @@
+all: up
+
+up:
+	docker-compose -f ./srcs/docker-compose.yml up -d --build
+
+down:
+	docker-compose -f ./srcs/docker-compose.yml down
+
+start:
+	docker-compose -f ./srcs/docker-compose.yml start
+
+stop:
+	docker-compose -f ./srcs/docker-compose.yml stop
+
+clean:
+	docker-compose -f ./srcs/docker-compose.yml down --volumes --rmi all
+
 cleanall:
 	sudo rm -rf /home/rreis-de42/data/wordpress/*
 	sudo rm -rf /home/rreis-de42/data/mysql/*
 	docker system prune -a --volumes
+
+re: cleanall all
+
+info:
+	@echo "Containers:"
+	docker ps -a
+	@echo "Images:"
+	docker images -a
+	@echo "Volumes:"
+	docker volume ls
+	@echo "Networks:"
+	docker network ls
+
+connect:
+	docker exec -it mariadb mysql -u root -p
+
+PHONY: all up down start stop clean cleanall info connect
--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 echo "hALOOOOOOOOOOOOOOOOOOOOOOO"
-# Check for existing installation
-# if [ ! -d "/run/mysqld" ]; then
-# 	mkdir -p /run/mysqld
-# 	chown -R mysql:mysql /run/mysqld
-# fi
 
 if [ ! -d "/var/lib/mysql/$MDB_NAME" ]; then
 
@@ -12,7 +7,6 @@ service mariadb start
 
 sleep 1
 
-#chown -R mysql:mysql /var/lib/mysql
 mysql_secure_installation << END
 
 Y
@@ -33,15 +27,6 @@ mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MDB_ROOT_PASSWOR
 mysql -u root -p$MDB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$MDB_ROOT_PASSWORD shutdown
 
-
-# Initialize mariadb in bootstrap mode (with initial data)
-#/usr/sbin/mysqld --user=mysql --bootstrap < $tfile
-#rm -f $tfile
-
-# Allow remote connections to the database
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# Finally, it starts the MariaDB server in console mode, using the 'mysql' user.
 else
     sleep 1
 fi
